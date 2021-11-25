1f82c667bc28401f1aefce671d21991942cd3694
izak <izabkru88@gmail.com>
2021-11-25 12:23:14 +0000

    - add pick_game_level method to Player class to allow the player to choose the game level - change functions name in run.py file to specify the operation of the functions

--- player.py
+++ player.py
@@ -2,4 +2,25 @@ class Player:
     def __init__(self, name) -> None:
         self.name = name
         self.score = 0
+        self.game_level = None
 
+    def pick_game_level(self):
+        level_menu = {
+            1: "Normal",
+            2: "Expert",
+            3: "Exit",
+        }
+        for key, value in level_menu.items():
+            print(f"{key} - {value}")
+        while True:
+            try:
+                level = int(input("Enter your choice: "))
+                if level > 3 or level < 1:
+                    raise ValueError
+                if level == 3:
+                    break
+                self.game_level = level
+                print(f"\nYou picked level {level_menu[level]}")
+                return level
+            except ValueError:
+                print("Please enter a valid number from 1 to 2.")
--- run.py
+++ run.py
@@ -1,25 +1,26 @@
 from player import Player
 
 
-def show_menu():
+def handle_menu():
     menu = {
         1: "New Game",
         2: "Best Scores",
         3: "Exit",
     }
     for key, value in menu.items():
-        print(f"{key} -- {value}")
+        print(f"{key} - {value}")
+    return input("Enter your choice: ")
 
 
 def new_game():
     name = str(input("What is your name?  "))
     player = Player(name)
-
-    print(f"\nHello {player.name}, welcome to BRAINSTORM QUIZ!") 
-    print("You will be presented with 10 questions.")
+    print(f"\nHello {player.name}, welcome to BRAINSTORM QUIZ!")
+    print("\nPlease choose a game level")
+    player.pick_game_level()
+    print("\nYou will be presented with 10 questions.")
     print("Enter the appropriate number to answer the question")
     print("Good luck!")
-    
     while True:
         break
 
@@ -28,13 +29,11 @@ def best_scores():
     print("Option 'Best Scores'")
 
 
-def manage_options():
-    print("MENU:")
+def manage_menu_options():
+    print("\nMENU:")
     while True:
-        show_menu()
-        option = ""
         try:
-            option = int(input("Enter your choice: "))
+            option = int(handle_menu())
         except ValueError:
             print("Please enter a valid number. From 1 to 3.")
             continue
@@ -51,4 +50,4 @@ def manage_options():
 
 
 if __name__ == "__main__":
-    manage_options()
+    manage_menu_options()
