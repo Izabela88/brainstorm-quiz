652e308b99c62b9396229e9d1069905d6f2c7a69
izak <izabkru88@gmail.com>
2021-12-16 18:27:47 +0000

    Add docstrings to questions.py

--- questions.py
+++ questions.py
@@ -10,11 +10,17 @@ console = Console()
 
 class Questions:
     def __init__(self, qty: int) -> None:
+        """Construct method
+
+        :param qty: Number of game questions
+        :type qty: int
+        """
         self.qty = qty
         self.game_questions = []
         self.question_number = 1
 
     def show_question_no(self) -> None:
+        """Display pannel with question number"""
         show_question_number = Panel.fit(
             Markdown(
                 f"\nQUESTION NO. {self.question_number}:", justify="center"
@@ -24,7 +30,12 @@ class Questions:
         )
         console.print(show_question_number)
 
-    def draw_questions(self) -> None:
+    def draw_questions(self) -> list:
+        """Method will load all questions and then draw unique questions
+
+        :return: return list with questions
+        :rtype: list
+        """
         raw_questions = load_from_file()
         for i in random.sample(raw_questions, k=self.qty):
             question = Question(
@@ -33,7 +44,12 @@ class Questions:
             self.game_questions.append(question)
         return self.game_questions
 
-    def next_question(self) -> None:
+    def next_question(self) -> "Question":
+        """Get next question, last from the list is removed
+
+        :return: Single Question
+        :rtype: Question
+        """
         return self.game_questions.pop()
 
 
@@ -41,12 +57,27 @@ class Question:
     def __init__(
         self, question: str, answers: List[str], correct_answer: int
     ) -> None:
+        """Question construct
+
+        :param question: Content of the question
+        :type question: str
+        :param answers: List of all answers
+        :type answers: List[str]
+        :param correct_answer: Index of the correct answer
+        :type correct_answer: int
+        """
         self.question = question
         self.answers = answers
         self.correct_answer = correct_answer
         self.answers_mapping = {"a": 0, "b": 1, "c": 2, "d": 3}
 
     def print_question(self, print_only: list = None) -> None:
+        """Display question
+
+        :param print_only: Print only answers that are in the list. This
+            argument is used by lifeline, defaults to None
+        :type print_only: list, optional
+        """
         show_question = Panel.fit(
             Markdown(f"\n{self.question}\n", justify="center"),
             width=60,
@@ -60,11 +91,21 @@ class Question:
             print(f"{list(self.answers_mapping.keys())[idx]}: {i}\n")
 
     def is_answer_correct(self, player_answer: int) -> bool:
+        """Check if player answer is right
+
+        :param player_answer: Player pick
+        :type player_answer: int
+        :return: Return True if answer is correct, else False
+        :rtype: bool
+        """
         if self.answers_mapping.get(player_answer) == self.correct_answer:
             return True
         return False
 
     def lifeline(self) -> None:
+        """Handles lifline logic. Method will remove two incorrect answers and
+        call another method to print again question with only two answers
+        """
         answers_copy = list(self.answers)
         for idx, _ in enumerate(answers_copy):
             answers_copy[idx] = idx
@@ -75,5 +116,10 @@ class Question:
 
 
 def load_from_file() -> list:
+    """Load all question from JSON file
+
+    :return: List of questions
+    :rtype: list
+    """
     with open("questions.json", "r") as json_file:
         return json.load(json_file)
