62a65d846a1be13a54981fc4fc8b661156227881
izak <izabkru88@gmail.com>
2021-12-16 17:56:51 +0000

    Add docstrings to score_board.py

--- score_board.py
+++ score_board.py
@@ -12,8 +12,14 @@ console = Console()
 
 
 class ScoreBoard:
-    def __init__(self, player: Player = None) -> None:
+    """ScoreBoard class - player scores"""
 
+    def __init__(self, player: Player = None) -> None:
+        """Construct method
+
+        :param player: Game player, defaults to None
+        :type player: Player, optional
+        """
         if player:
             self.game_level = player.game_level
             self.player_name = player.name
@@ -21,6 +27,7 @@ class ScoreBoard:
             self.game_time = player.game_time
 
     def save_score(self) -> None:
+        """Build up player score and call function to save this to file"""
         player_score = {
             "id": str(uuid.uuid4()),
             "player_name": self.player_name,
@@ -32,10 +39,17 @@ class ScoreBoard:
             best_scores[self.game_level].append(player_score)
         else:
             best_scores[self.game_level] = [player_score]
-        best_scores[self.game_level] = self.sort_scores(best_scores[self.game_level])
-        self.save_best_scores(best_scores)
+        best_scores[self.game_level] = self.sort_scores(
+            best_scores[self.game_level]
+        )
+        self.save_to_file(best_scores)
 
     def is_score_qualified(self) -> bool:
+        """Function determine if player score is good enough to be saved
+
+        :return: True if user qualifies, else False
+        :rtype: bool
+        """
         is_qualified = False
 
         best_scores = self.get_best_scores()
@@ -55,7 +69,7 @@ class ScoreBoard:
             is_qualified = True
 
         best_scores[self.game_level] = level_scores_sorted
-        self.save_best_scores(best_scores)
+        self.save_to_file(best_scores)
         if is_qualified is False:
             console.print(
                 f"[bold yellow1]\nWell...{self.player_name}"
@@ -70,12 +84,15 @@ class ScoreBoard:
         return is_qualified
 
     def show_best_scores(self) -> None:
+        """Display best scores"""
         best_scores = self.get_best_scores()
         if not best_scores:
             console.print("\nTHERE ARE NO RESULTS YET!!\n", style="dark_blue")
         for k, v in best_scores.items():
             score_title = Panel.fit(
-                Markdown(f"\n10 {k.upper()} LEVEL BEST SCORES:\n", justify="center"),
+                Markdown(
+                    f"\n10 {k.upper()} LEVEL BEST SCORES:\n", justify="center"
+                ),
                 width=60,
                 style="bold dark_blue",
             )
@@ -91,6 +108,11 @@ class ScoreBoard:
 
     @staticmethod
     def get_best_scores() -> dict:
+        """Load best scores from json file
+
+        :return: Best scores object
+        :rtype: dict
+        """
         with open("best_scores.json", "r") as f:
             try:
                 scores = json.load(f)
@@ -99,12 +121,24 @@ class ScoreBoard:
         return scores
 
     @staticmethod
-    def save_best_scores(best_scores: dict) -> None:
+    def save_to_file(best_scores: dict) -> None:
+        """Save best scores to json file
+
+        :param best_scores: [description]
+        :type best_scores: dict
+        """
         with open("best_scores.json", "w") as f:
             json.dump(best_scores, f)
 
     @staticmethod
     def sort_scores(scores: list) -> list:
+        """Sort players scores by score and game time
+
+        :param scores: Players best scores for one game level
+        :type scores: list
+        :return: Sorted scores
+        :rtype: list
+        """
         return sorted(
             scores,
             key=lambda i: (i["player_score"], -i["player_game_time"]),
